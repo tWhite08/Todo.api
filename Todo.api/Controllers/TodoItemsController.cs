78c9f5bb94670f43294e5d1372b1f37acca54b35
Taylor White <taylorwhite1003@gmail.com>
2020-03-03 20:11:46 -0500

    Initial Push

---
+++ Todo.api/Controllers/TodoItemsController.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Todo.Api.Models;
+using Todo.Api.Services;
+using System;
+
+namespace Todo.Api.Controllers
+{
+    [ApiController]
+    [Route("api/todos")]
+    public class TodoItemsController : ControllerBase
+    {
+        private ITodoRepository repository;
+        public TodoItemsController(ITodoRepository todoRepository)
+        {
+            repository = todoRepository ??
+                throw new ArgumentNullException(nameof(todoRepository));
+        }
+        
+        [HttpGet]
+        public IActionResult GetTodos()
+        {
+            var result = repository.GetAllTodoItems();
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+    }
+}
--- Todo.api/Startup.cs
+++ Todo.api/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Todo.Api.Services;
 
 namespace Todo.api
 {
@@ -26,6 +27,7 @@ namespace Todo.api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<ITodoRepository, TodoRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
